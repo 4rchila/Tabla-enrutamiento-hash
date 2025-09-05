7a3a0d7bc71749ec1648d5119a49c2d5d76cb39c
Pablo Lorenzo <apjlorenzo1@gmail.com>
2025-09-04 19:14:06 -0600

    Manejo de colisiones con sondeo lineal
    Prueba en el main

--- src/hash_tabla.py
+++ src/hash_tabla.py
@@ -1,5 +1,5 @@
 class HashTable:
-    def __init__(self, size=10):
+    def __init__(self, size=5):
         self.size = size
         self.table = [None] * size
 
@@ -13,15 +13,35 @@ class HashTable:
 
     def add_route(self, ip: str, interfaz: str):
         index = self.hash_ip(ip)
+        start_index = index  # Se guarda el idnice inicial para detener bucles infinitos
+
+        while self.table[index] is not None:
+            # Si la IP ya está registrada, se actualiza
+            if self.table[index][0] == ip:
+                self.table[index] = (ip, interfaz)
+                print(f"Actualizada {ip} -> {interfaz} en índice {index}")
+                return
+            # Si no es la misma IP, se prueba la siguiente casilla
+            index = (index + 1) % self.size
+            if index == start_index:
+                print("La tabla está llena, no se puede insertar.")
+                return
+
+        # Encontramos un espacio vacío
         self.table[index] = (ip, interfaz)
         print(f"Agregado {ip} -> {interfaz} en índice {index}")
 
     def find_router(self, ip: str):
-        for d in self.table:
-            if d is not None:
-                if ip == d[0]:
-                    print(f"La Ip: {ip}, fue encontrada exitosamente")
-                    return
-                else:
-                    print(f"La Ip: {ip}, no se encuentra registrada")
+        index = self.hash_ip(ip)
+        start_index = index
 
+        while self.table[index] is not None:
+            if self.table[index][0] == ip:
+                print(f"La IP {ip} fue encontrada en índice {index} -> Interfaz: {self.table[index][1]}")
+                return self.table[index][1]
+            index = (index + 1) % self.size
+            if index == start_index:  # se dio toda la vuelta
+                break
+
+        print(f"La IP {ip} no se encuentra registrada")
+        return None
